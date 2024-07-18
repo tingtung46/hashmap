8bec0e2bd1994dcf2204675b5a613bfbf5642ce4
root <132725436+tingtung46@users.noreply.github.com>
2024-07-18 20:21:28 +0700

    Construct hashSet and update testing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import HashMap from "./hashMap.js";
+import HashSet from "./hashSet.js";
 
 const test = new HashMap();
 
@@ -24,3 +25,27 @@ console.log(test.length());
 console.log(test.keys());
 console.log(test.values());
 console.log(test.entries());
+
+const test2 = new HashSet();
+
+test2.set("apple");
+test2.set("banana");
+test2.set("carrot");
+test2.set("dog");
+test2.set("elephant");
+test2.set("frog");
+test2.set("grape");
+test2.set("hat");
+test2.set("ice cream");
+test2.set("jacket");
+test2.set("kite");
+test2.set("lion");
+test2.set("moon");
+
+console.log(test2.table);
+console.log(test2.has("ice cream"));
+console.log(test2.remove("banana"));
+console.log(test2.get("hat"));
+console.log(test2.length());
+console.log(test2.keys());
+console.log(test2.entries());
